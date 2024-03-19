c96249e560acfcfba13ec6302fed663e388919d8
Lera <lerakachanovskaya@gmail.com>
2024-03-19 12:56:50 +0300

    Add students unique trigger

--- Lab2/lab2.sql
+++ Lab2/lab2.sql
@@ -13,3 +13,23 @@ CREATE TABLE groups (
 
     CONSTRAINT groups_pk PRIMARY KEY (id)
 );
+
+CREATE OR REPLACE TRIGGER check_student_id_unique
+BEFORE INSERT OR UPDATE
+    ON students
+    FOR EACH ROW
+DECLARE
+    v_count NUMBER;
+BEGIN
+    SELECT count(*) INTO v_count
+    FROM students WHERE id = :NEW.id;
+
+    IF (v_count > 0) THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Запись с таким ID уже существует!');
+    end if;
+
+end;
+
+INSERT INTO students (id, name, group_id) VALUES (1, 'Lera', 1);
+SELECT * FROM students;
+
